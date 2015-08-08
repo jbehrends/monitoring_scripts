476c9cd04c5e898f8ae02d4d497d4306a8d1dc5b
Josh Behrends <josh@behrends.us>
2015-08-07 17:58:02 -0700

    Adding memory statistics

--- graphite/edgerouter_metrics.sh
+++ graphite/edgerouter_metrics.sh
@@ -22,6 +22,11 @@ sysUpTime=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.2.1.1.3 | sed 
 load1=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.10.1.3.1 | awk '{print $4}'| tr -d '"')
 load5=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.10.1.3.2 | awk '{print $4}'| tr -d '"')
 load15=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.10.1.3.3 | awk '{print $4}'| tr -d '"')
+memTotalReal=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.4.5 | awk '{print $4}')
+memAvailReal=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.4.6 | awk '{print $4}')
+memBuffer=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.4.14 | awk '{print $4}')
+memShared=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.4.13 | awk '{print $4}')
+memCached=$(snmpwalk -On -v 2c -c $SNMP_Community $HostIP 1.3.6.1.4.1.2021.4.15 | awk '{print $4}')
 
 # output to graphite from walked metrics above.
 for (( i=0; i<${#interface[*]}; i=i+1 )); do
@@ -33,5 +38,9 @@ echo "$MetricRoot"."$Host".load.load-1" $load1 `date +%s`" | nc ${CarbonServer} 
 echo "$MetricRoot"."$Host".load.load-5" $load5 `date +%s`" | nc ${CarbonServer} ${CarbonPort};
 echo "$MetricRoot"."$Host".load.load-15" $load15 `date +%s`" | nc ${CarbonServer} ${CarbonPort};
 echo "$MetricRoot"."$Host".uptime.seconds" $(($sysUpTime/100)) `date +%s`" | nc ${CarbonServer} ${CarbonPort};
-
+echo "$MetricRoot"."$Host".memory.memTotalReal" $memTotalReal `date +%s`" | nc ${CarbonServer} ${CarbonPort};
+echo "$MetricRoot"."$Host".memory.memAvailReal" $memAvailReal `date +%s`" | nc ${CarbonServer} ${CarbonPort};
+echo "$MetricRoot"."$Host".memory.memBuffer" $memBuffer `date +%s`" | nc ${CarbonServer} ${CarbonPort};
+echo "$MetricRoot"."$Host".memory.memShared" $memShared `date +%s`" | nc ${CarbonServer} ${CarbonPort};
+echo "$MetricRoot"."$Host".memory.memCached" $memCached `date +%s`" | nc ${CarbonServer} ${CarbonPort};
 
